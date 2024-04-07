cdd3f7604359e29cfedd51cfad0e13c7f4b38059
Senkou <seruteitor@gmail.com>
2024-04-07 23:04:59 +0200

    Ejercicio 2.6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,28 +4,26 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
 
 class AppState {
    val text = mutableStateOf("")
    val buttonEnabled: Boolean
       get() = text.value.isNotEmpty()
 }
+
 @Composable
 @Preview
 fun App(appState: AppState) {
-//   var text by remember { mutableStateOf("") }
-//   val message = buldMessage(text)
-//   val buttonEnabled = text.isNotEmpty()
 
    MaterialTheme {
       Column {
          TextField(value = appState.text.value, onValueChange = { appState.text.value = it })
          Text(text = buildMessage(appState.text.value))
-         Button(onClick = {appState.text.value = "" }, enabled = appState.buttonEnabled) {
+         Button(onClick = { appState.text.value = "" }, enabled = appState.buttonEnabled) {
             Text("Clean")
          }
       }
