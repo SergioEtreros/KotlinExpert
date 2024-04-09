e6e14c600ef1b6bc7605a2fc8203d5b2be05ce33
Senkou <seruteitor@gmail.com>
2024-04-09 17:39:22 +0200

    Ejercicio 2.14 Lambdas

--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -1,13 +1,19 @@
-import Note.*
+import Note.Type
 
 data class Note(val title: String, val description: String, val type: Type) {
    enum class Type { TEXT, AUDIO }
 }
 
-fun getNotes(): List<Note> = (1..10).map {
-   Note(
-      "Title $it",
-      "Description $it",
-      if (it % 3 == 0) Type.AUDIO else Type.TEXT
-   )
+fun getNotes(callback: (List<Note>) -> Unit) {
+
+   val notes = (1..10).map {
+      Note(
+         "Title $it",
+         "Description $it",
+         if (it % 3 == 0) Type.AUDIO else Type.TEXT
+      )
+   }
+   Thread.sleep(2000)
+   callback(notes)
 }
+
